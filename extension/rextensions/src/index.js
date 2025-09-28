6bc1d11350b484344b1f966c2c2cabc2ce1aafb9
Shubh Mittal <shubhmittal8971@gmail.com>
2025-09-28 15:15:00 +0530

    Update core extension files with tested local versions

--- extension/rextensions/src/index.js
+++ extension/rextensions/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Popup from './popup';
+import Popup from './Popup.jsx';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
