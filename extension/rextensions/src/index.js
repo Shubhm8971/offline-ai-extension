ae1ca965bc59584e9adfb6535e17a5be4b9b26d4
Shubh Mittal <shubhmittal8971@gmail.com>
2025-09-28 13:27:08 +0530

    Update index.js

--- extension/rextensions/src/index.js
+++ extension/rextensions/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Popup from './popup';
+import Popup from './popup.jsx';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
